26fa81cc663603a4704b6051df214d163a065396
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-07-04 11:24:48 +0100

    SAOD-336 adding the tests back for check your answers page

--- test/controllers/CheckYourAnswersControllerSpec.scala
+++ test/controllers/CheckYourAnswersControllerSpec.scala
@@ -14,16 +14,16 @@
  * limitations under the License.
  */
 
-package controllers
-
 import base.SpecBase
+import controllers.routes
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
+import play.api.test.Helpers.*
+import viewmodels.govuk.SummaryListFluency
 import views.html.CheckYourAnswersView
 
-class CheckYourAnswersControllerSpec extends SpecBase {
+class CheckYourAnswersControllerSpec extends SpecBase with SummaryListFluency {
 
-  "CheckYourAnswers Controller" - {
+  "Check Your Answers Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
@@ -35,8 +35,24 @@ class CheckYourAnswersControllerSpec extends SpecBase {
         val result = route(application, request).value
 
         val view = application.injector.instanceOf[CheckYourAnswersView]
+        val list = SummaryListViewModel(Seq.empty)
 
         status(result) mustEqual OK
+        contentAsString(result) mustEqual view(list)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.CheckYourAnswersController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }
   }
