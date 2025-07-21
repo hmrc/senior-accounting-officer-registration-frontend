f2aa24aaf50a7ce4838f8b25b863eaceafd45a53
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-07-21 17:19:10 +0100

    SAOD-395 updated/added view, specs, messages

--- app/controllers/RegistrationCompleteController.scala
+++ app/controllers/RegistrationCompleteController.scala
@@ -33,7 +33,7 @@ class RegistrationCompleteController @Inject() (
 ) extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+  def onPageLoad: Action[AnyContent] = identify { implicit request =>
     Ok(view())
   }
 
