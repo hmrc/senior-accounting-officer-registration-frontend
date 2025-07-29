c7c34ea7eabdb247a8d0d7b291d61b258737d36e
cyborgaeon <38399164+CyborgAeon@users.noreply.github.com>
2025-07-29 09:59:37 +0100

    Implement contact check answers service to resolve flatmap for input user answers.

---
+++ app/services/ContactCheckYourAnswersService.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import models.*
+import pages.*
+
+class ContactCheckYourAnswersService {
+
+  def getContactInfos(userAnswers: UserAnswers): List[ContactInfo] = { 
+    ContactType.values.flatMap {
+      contactType => getContactInfo(userAnswers, contactType)
+    }.toList
+    
+  }
+
+  private[services] def getContactInfo(userAnswers: UserAnswers, contactType: ContactType): Option[ContactInfo] = 
+    for {
+      name <- userAnswers.get(ContactNamePage(contactType))
+      role <- userAnswers.get(ContactRolePage(contactType))
+      email <- userAnswers.get(ContactEmailPage(contactType))
+      phone <- userAnswers.get(ContactPhonePage(contactType))
+    } yield ContactInfo(name, role, email, phone)
+}
