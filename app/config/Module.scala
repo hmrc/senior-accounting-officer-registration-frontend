dc1fe26b4f259bfd954004492b7dcfa032de262f
cyborgaeon <38399164+CyborgAeon@users.noreply.github.com>
2025-08-12 14:55:10 +0100

    add whitespace back

--- app/config/Module.scala
+++ app/config/Module.scala
@@ -27,6 +27,7 @@ class Module extends AbstractModule {
 
     bind(classOf[DataRetrievalAction]).to(classOf[DataRetrievalActionImpl]).asEagerSingleton()
     bind(classOf[DataRequiredAction]).to(classOf[DataRequiredActionImpl]).asEagerSingleton()
+    
     bind(classOf[IdentifierAction]).to(classOf[FrontendAuthenticatedIdentifierAction]).asEagerSingleton()
     bind(classOf[ApiAuthenticatedIdentifierAction]).to(classOf[ApiAuthenticatedIdentifierActionImpl]).asEagerSingleton()
 
