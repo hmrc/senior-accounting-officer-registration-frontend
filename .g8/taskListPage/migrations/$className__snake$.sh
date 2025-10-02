2bceb37359d93564941f9c7f9cf1a289dc851942
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-02 15:44:49 +0100

    wip testing tasklist

---
+++ .g8/taskListPage/migrations/$className__snake$.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration $className;format="snake"$"
+
+echo "Adding routes to conf/app.routes"
+echo "" >> ../conf/app.routes
+echo "GET        /$className;format="decap"$                       controllers.$className$Controller.onPageLoad()" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "$className;format="decap"$.title = $className;format="decap"$" >> ../conf/messages.en
+echo "$className;format="decap"$.heading = $className;format="decap"$" >> ../conf/messages.en
+
+
+
+for i in $(seq 1 $numberOfTasks$); do
+  echo "$className;format="decap"$.task.$i.text = Task $i" >> ../conf/messages.en
+done
+
+echo ""
+echo "Migration $className;format="snake"$ completed"
