8f8d6e8db44e3460c8c86b018631ed724730f1ee
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-02 20:53:39 +0100

    updates

--- .g8/taskListPage/migrations/$className__snake$.sh
+++ .g8/taskListPage/migrations/$className__snake$.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+
+MESSAGES_FILE=../conf/messages.en
+ROUTES_FILE=../conf/app.routes
+
 echo ""
 echo "Applying migration $className;format="snake"$"
 
 echo "Adding routes to conf/app.routes"
-echo "" >> ../conf/app.routes
-echo "GET        /$className;format="decap"$                       controllers.$className$Controller.onPageLoad()" >> ../conf/app.routes
+{
+  echo ""
+  echo "GET        /$className;format="decap"$                       controllers.$className$Controller.onPageLoad()"
+}  >> "\$ROUTES_FILE"
 
 echo "Adding messages to conf.messages"
-echo "" >> ../conf/messages.en
-echo "$className;format="decap"$.title = $className;format="decap"$" >> ../conf/messages.en
-echo "$className;format="decap"$.heading = $className;format="decap"$" >> ../conf/messages.en
+{
+  echo ""
+  echo "$className;format="decap"$.title = $className;format="decap"$"
+  echo "$className;format="decap"$.heading = $className;format="decap"$"
+  echo "$className;format="decap"$.task.status.cannotStartYet = Cannot start yet"
+  echo "$className;format="decap"$.task.status.completed = Completed"
+  echo "$className;format="decap"$.task.status.notStarted = NotStarted"
+} >> "\$MESSAGES_FILE"
 
 
 i=1
 while [ \$i -le $numberOfTasks$ ]
 do
-  echo "$className;format="decap"$.task.\$i.text = Task \$i" >> ../conf/messages.en
+  echo "$className;format="decap"$.task.\$i.text = Task \$i" >> "\$MESSAGES_FILE"
   i=\$((i+1))
 done
-
-echo "$className;format="decap"$.task.status.cannotStartYet = Cannot start yet" >> ../conf/messages.en
-echo "$className;format="decap"$.task.status.completed = Completed" >> ../conf/messages.en
-echo "$className;format="decap"$.task.status.notStarted = NotStarted" >> ../conf/messages.en
-
 
 echo ""
 echo "Migration $className;format="snake"$ completed"
