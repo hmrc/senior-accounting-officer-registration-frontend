e1df8bc0fb7636eaaf52e08b671137f2705d17fb
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-02 14:13:44 +0100

    SAOD-446, adding first view spec for contentPage

---
+++ .g8/contentPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,25 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import play.api.i18n.Messages
+import views.html.$className$View
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val messageKeyPrefix = "$className;format="decap"$"
+  private val messageProvider: Messages = messages(app)
+  private val pageHeading: String = messageProvider(messageKeyPrefix + ".heading")
+
+  "$className$View" - {
+
+    "must generate a view" - {
+      val doc = Jsoup.parse(SUT().toString)
+      doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestForBackLink(show = true)
+      doc.createTestMustHaveCorrectPageHeading(pageHeading)
+    }
+
+  }
+}
