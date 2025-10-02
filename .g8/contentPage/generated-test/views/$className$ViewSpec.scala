01366bf024949d1194186c681ec7f8e44290540e
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-02 14:23:13 +0100

    SAOD-446, updates for view spec for contentPage

--- .g8/contentPage/generated-test/views/$className$ViewSpec.scala
+++ .g8/contentPage/generated-test/views/$className$ViewSpec.scala
@@ -11,14 +11,15 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
   private val messageKeyPrefix = "$className;format="decap"$"
   private val messageProvider: Messages = messages(app)
   private val pageHeading: String = messageProvider(messageKeyPrefix + ".heading")
+  private val pageTitle: String = messageProvider(messageKeyPrefix + ".title")
 
   "$className$View" - {
 
     "must generate a view" - {
       val doc = Jsoup.parse(SUT().toString)
       doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestMustHaveCorrectPageHeading(pageTitle)
       doc.createTestForBackLink(show = true)
-      doc.createTestMustHaveCorrectPageHeading(pageHeading)
     }
 
   }
