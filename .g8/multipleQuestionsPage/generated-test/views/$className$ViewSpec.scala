ebc42ab25e146f0232fd18e789aed0b8cd932599
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-06 13:10:14 +0100

    added multipleQuestionsPage viewspec

---
+++ .g8/multipleQuestionsPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,47 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.i18n.Messages
+import play.api.data.Form
+import forms.$className$FormProvider
+import models.$className$
+import models.NormalMode
+import pages.$className$Page
+import views.html.$className$View
+import views.$className$ViewSpec.*
+
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val formProvider = new $className$FormProvider()
+  lazy val form: Form[$className$] = formProvider()
+
+  private def generateView(form: Form[$className$]): Document = {
+    val view = SUT(form, NormalMode)
+    Jsoup.parse(view.toString)
+  }
+
+  "$className$View" - {
+    "when the form is empty (no errors)" - {
+      val doc = generateView(form)
+      doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestForBackLink(show = true)
+      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+
+      "must display the correct labels for fields" in {
+        doc.select("label[for=$field1Name$]").text() mustBe field1Label
+        doc.select("label[for=$field2Name$]").text() mustBe field2Label
+      }
+    }
+  }
+}
+
+object $className$ViewSpec {
+  val pageHeading = "$className;format="decap"$"
+  val pageTitle = "$className;format="decap"$"
+  val field1Label = "$field1Name;format="decap"$"
+  val field2Label = "$field2Name;format="decap"$"
+}
