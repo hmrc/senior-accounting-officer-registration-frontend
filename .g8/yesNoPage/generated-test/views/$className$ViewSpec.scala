ee7e3505f2bf2d79d9f525958f086099b8d35eeb
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-06 11:29:04 +0100

    added viewspec generation for yesNoPage

---
+++ .g8/yesNoPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,47 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.i18n.Messages
+import play.api.data.Form
+import forms.$className$FormProvider
+import models.NormalMode
+import pages.$className$Page
+import views.html.$className$View
+import views.$className$ViewSpec.*
+
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val formProvider = new $className$FormProvider()
+  lazy val form: Form[Boolean] = formProvider()
+  //private val messageKeyPrefix = "$className;format="decap"$"
+
+  private def generateView(form: Form[Boolean]): Document = {
+    val view = SUT(form, NormalMode)
+    Jsoup.parse(view.toString)
+  }
+
+  "$className$View" - {
+    "when the form is empty (no errors)" - {
+      val doc = generateView(form)
+      doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestForBackLink(show = true)
+      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+
+      "must display the correct radio buttons" in {
+        doc.select("label[for=value]").text() mustBe yesLabel
+        doc.select("label[for=value-no]").text() mustBe noLabel
+      }
+    }
+  }
+}
+
+object $className$ViewSpec {
+  val pageHeading = "$className;format="decap"$"
+  val pageTitle = "$className;format="decap"$"
+  val yesLabel = "Yes"
+  val noLabel = "No"
+}
