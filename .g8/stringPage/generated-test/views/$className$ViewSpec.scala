78c6493f0587ef69af8f705db6ba4b4c23b320c3
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-06 11:38:34 +0100

    added stringPage viewspec generation

---
+++ .g8/stringPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,44 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.i18n.Messages
+import play.api.data.Form
+import models.NormalMode
+import pages.$className$Page
+import forms.$className$FormProvider
+import views.html.$className$View
+import views.$className$ViewSpec.*
+
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val formProvider = new $className$FormProvider()
+  lazy val form: Form[String] = formProvider()
+
+  private def generateView(form: Form[String]): Document = {
+    val view = SUT(form, NormalMode)
+    Jsoup.parse(view.toString)
+  }
+
+  "$className$View" - {
+    "when the form is empty (no errors)" - {
+      val doc = generateView(form)
+      doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestForBackLink(show = true)
+      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+
+      "must display the correct label" in {
+        doc.select("label[for=value]").text() mustBe pageTitle
+      }
+    }
+  }
+}
+
+object $className$ViewSpec {
+  val pageHeading = "$className;format="decap"$"
+  val pageTitle = "$className;format="decap"$"
+}
+
