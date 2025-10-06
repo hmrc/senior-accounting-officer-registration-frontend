0295cf3ac2d0c5ef080aa8c39dd66586f34ee1d7
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-06 12:25:02 +0100

    adding currencyPage viewspec

---
+++ .g8/currencyPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,43 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.data.Form
+import models.NormalMode
+import pages.$className$Page
+import forms.$className$FormProvider
+import views.html.$className$View
+import views.$className$ViewSpec.*
+
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val formProvider = new $className$FormProvider()
+  lazy val form: Form[BigDecimal] = formProvider()
+
+  private def generateView(form: Form[BigDecimal]): Document = {
+    val view = SUT(form, NormalMode)
+    Jsoup.parse(view.toString)
+  }
+
+  "$className$View" - {
+    "when the form is empty (no errors)" - {
+      val doc = generateView(form)
+      doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestForBackLink(show = true)
+      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+
+      "must display the correct label" in {
+        doc.select("label[for=value]").text() mustBe pageTitle
+      }
+    }
+  }
+}
+
+
+object $className$ViewSpec {
+  val pageHeading = "$className$"
+  val pageTitle = "$className$"
+}
