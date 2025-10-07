0b91358026e8298fc25af66debe7b3c303dcc243
Rishi Sachania <19263328+rishisachania@users.noreply.github.com>
2025-10-07 11:39:15 +0100

    Updates to var mapping

--- .g8/intPage/generated-test/views/$className$ViewSpec.scala
+++ .g8/intPage/generated-test/views/$className$ViewSpec.scala
@@ -39,6 +39,6 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
 
 
 object $className$ViewSpec {
-  val pageHeading = "$className$"
-  val pageTitle = "$className$"
+  val pageHeading = "$title$"
+  val pageTitle = "$title$"
 }
