10599f508ea8c16172a9fb5c011debfc891c4cea
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-07 10:32:12 +0100

    updates

--- .g8/datePage/generated-test/views/$className$ViewSpec.scala
+++ .g8/datePage/generated-test/views/$className$ViewSpec.scala
@@ -5,6 +5,7 @@ import base.ViewSpecBase
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.api.data.Form
+import play.api.inject.Injector
 import models.NormalMode
 import pages.$className$Page
 import forms.$className$FormProvider
@@ -15,7 +16,7 @@ import java.time.LocalDate
 
 class $className$ViewSpec extends ViewSpecBase[$className$View] {
 
-  private val formProvider = new $className$FormProvider()
+  private val formProvider = app.injector.instanceOf[$className$FormProvider]
   private val form: Form[LocalDate] = formProvider()
 
   private def generateView(form: Form[LocalDate]): Document = {
