00cd8e96ccfd537f2397b96d57d72fc4a4fa486d
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-08 17:14:30 +0100

    updates

--- .g8/stringPage/generated-test/views/$className$ViewSpec.scala
+++ .g8/stringPage/generated-test/views/$className$ViewSpec.scala
@@ -6,7 +6,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.api.inject.Injector
 import play.api.data.Form
-import models.NormalMode
+import models.{NormalMode, CheckMode, Mode}
 import pages.$className$Page
 import forms.$className$FormProvider
 import views.html.$className$View
@@ -18,21 +18,35 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
   private val formProvider = app.injector.instanceOf[$className$FormProvider]
   private val form: Form[String] = formProvider()
 
-  private def generateView(form: Form[String]): Document = {
-    val view = SUT(form, NormalMode)
+  private def generateView(form: Form[String], mode: Mode = NormalMode): Document = {
+    val view = SUT(form, mode)
     Jsoup.parse(view.toString)
   }
 
   "$className$View" - {
-    "when the form is empty (no errors)" - {
+    "when the form is empty (no errors), NormalMode" - {
       val doc = generateView(form)
       doc.mustHaveCorrectPageTitle(pageHeading)
       doc.createTestForBackLink(show = true)
       doc.createTestMustHaveCorrectPageHeading(pageTitle)
+      doc.createTestMustShowIsThisPageNotWorkingProperlyLink
 
       "must display the correct label" in {
         doc.select("label[for=value]").text() mustBe pageTitle
       }
+
+      doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+        controllers.routes.$className$Controller.onSubmit(NormalMode),
+        "Continue"
+      )
+    }
+    "when using CheckMode" - {
+      val doc = generateView(form, CheckMode)
+
+      doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+        controllers.routes.$className$Controller.onSubmit(CheckMode),
+        "Continue"
+      )
     }
   }
 }
