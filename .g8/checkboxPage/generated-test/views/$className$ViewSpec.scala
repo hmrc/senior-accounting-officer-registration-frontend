b2cef661126ddc12171c785f3a5b138bed829c60
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-08 15:33:13 +0100

    updates

--- .g8/checkboxPage/generated-test/views/$className$ViewSpec.scala
+++ .g8/checkboxPage/generated-test/views/$className$ViewSpec.scala
@@ -8,7 +8,7 @@ import play.api.inject.Injector
 import play.api.data.Form
 import forms.$className$FormProvider
 import models.$className$
-import models.NormalMode
+import models.{NormalMode, CheckMode, Mode}
 import pages.$className$Page
 import views.html.$className$View
 import views.$className$ViewSpec.*
@@ -19,8 +19,8 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
   private val formProvider = app.injector.instanceOf[$className$FormProvider]
   private val form: Form[Set[$className$]] = formProvider()
 
-  private def generateView(form: Form[Set[$className$]]): Document = {
-    val view = SUT(form, NormalMode)
+  private def generateView(form: Form[Set[$className$]], mode: Mode = NormalMode): Document = {
+    val view = SUT(form, mode)
     Jsoup.parse(view.toString)
   }
 
@@ -36,6 +36,13 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
       "must display the correct checkbox labels" in {
         doc.getMainContent.select("label[for=value_0]").text() mustBe option1Label
         doc.getMainContent.select("label[for=value_1]").text() mustBe option2Label
+      }
+    }
+    "when using CheckMode" - {
+      val doc = generateView(form, CheckMode)
+      "must have form which submits to correct action" in {
+        val form = doc.select("form")
+        form.attr("action") mustBe controllers.routes.$className$Controller.onSubmit(CheckMode).url
       }
     }
   }
