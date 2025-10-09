3cca25e001f80ab2f025a7759979a21de4fb4493
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-09 10:50:04 +0100

    Revert "updates"
    This reverts commit 1d16b30ccba8c6205c063931a7c9a1bff5553b21.

---
+++ .g8/radioButtonPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,61 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.inject.Injector
+import play.api.data.Form
+import forms.$className$FormProvider
+import models.$className$
+import models.{NormalMode, CheckMode, Mode}
+import pages.$className$Page
+import views.html.$className$View
+import views.$className$ViewSpec.*
+
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val formProvider = app.injector.instanceOf[$className$FormProvider]
+  private val form: Form[$className$] = formProvider()
+
+  private def generateView(form: Form[$className$], mode: Mode = NormalMode): Document = {
+    val view = SUT(form, mode)
+    Jsoup.parse(view.toString)
+  }
+
+  "$className$View" - {
+    "when the form is empty (no errors), NormalMode" - {
+      val doc = generateView(form)
+      doc.mustHaveCorrectPageTitle(pageHeading)
+      doc.createTestForBackLink(show = true)
+      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+      doc.createTestMustShowIsThisPageNotWorkingProperlyLink
+
+      "must display the correct radio button lables" in {
+        doc.select("label[for=value_0]").text() mustBe option1Label
+        doc.select("label[for=value_1]").text() mustBe option2Label
+      }
+
+      doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+        controllers.routes.$className$Controller.onSubmit(NormalMode),
+        "Continue"
+      )
+    }
+    "when using CheckMode" - {
+      val doc = generateView(form, CheckMode)
+
+      doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+        controllers.routes.$className$Controller.onSubmit(CheckMode),
+        "Continue"
+      )
+    }
+  }
+}
+
+object $className$ViewSpec {
+  val pageHeading = "$title$"
+  val pageTitle = "$title$"
+  val option1Label = "$option1msg$"
+  val option2Label = "$option2msg$"
+}
