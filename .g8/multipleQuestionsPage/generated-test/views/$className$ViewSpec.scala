d0dd4b0076ab9dc0abda02b5e189d54388b71037
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-10-09 14:20:17 +0100

    updates

--- .g8/multipleQuestionsPage/generated-test/views/$className$ViewSpec.scala
+++ .g8/multipleQuestionsPage/generated-test/views/$className$ViewSpec.scala
@@ -8,7 +8,7 @@ import play.api.inject.Injector
 import play.api.data.Form
 import forms.$className$FormProvider
 import models.$className$
-import models.NormalMode
+import models.{NormalMode, CheckMode, Mode}
 import pages.$className$Page
 import views.html.$className$View
 import views.$className$ViewSpec.*
@@ -19,22 +19,36 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
   private val formProvider = app.injector.instanceOf[$className$FormProvider]
   private val form: Form[$className$] = formProvider()
 
-  private def generateView(form: Form[$className$]): Document = {
-    val view = SUT(form, NormalMode)
+  private def generateView(form: Form[$className$], mode: Mode): Document = {
+    val view = SUT(form, mode)
     Jsoup.parse(view.toString)
   }
 
-  "$className$View" - {
-    "when the form is empty (no errors)" - {
-      val doc = generateView(form)
-      doc.mustHaveCorrectPageTitle(pageHeading)
-      doc.createTestForBackLink(show = true)
-      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+  private def doChecks(doc: Document, mode: Mode): Unit = {
+    doc.mustHaveCorrectPageTitle(pageHeading)
+    doc.createTestForBackLink(show = true)
+    doc.createTestMustHaveCorrectPageHeading(pageTitle)
+    doc.createTestMustShowIsThisPageNotWorkingProperlyLink
 
-      "must display the correct labels for fields" in {
-        doc.select("label[for=$field1Name$]").text() mustBe field1Label
-        doc.select("label[for=$field2Name$]").text() mustBe field2Label
-      }
+    "must display the correct labels for fields" in {
+      doc.select("label[for=$field1Name$]").text() mustBe field1Label
+      doc.select("label[for=$field2Name$]").text() mustBe field2Label
+    }
+
+    doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+      controllers.routes.$className$Controller.onSubmit(mode),
+      "Continue"
+    )
+  }
+
+  "$className$View" - {
+    "when using NormalMode, when the form is empty (no errors)" - {
+      val doc = generateView(form, NormalMode)
+      doChecks(doc, NormalMode)
+    }
+    "when using CheckMode" - {
+      val doc = generateView(form, CheckMode)
+      doChecks(doc, CheckMode)
     }
   }
 }
