221105ecec786377f8901087cb673a49f1e4b382
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2025-10-21 13:32:58 +0100

    test: test the mode of the page

--- .g8/currencyPage/generated-test/views/$className$ViewSpec.scala
+++ .g8/currencyPage/generated-test/views/$className$ViewSpec.scala
@@ -11,6 +11,7 @@ import pages.$className$Page
 import forms.$className$FormProvider
 import views.html.$className$View
 import views.$className$ViewSpec.*
+import models.Mode
 
 
 class $className$ViewSpec extends ViewSpecBase[$className$View] {
@@ -18,29 +19,36 @@ class $className$ViewSpec extends ViewSpecBase[$className$View] {
   private val formProvider = app.injector.instanceOf[$className$FormProvider]
   private val form: Form[BigDecimal] = formProvider()
 
-  private def generateView(form: Form[BigDecimal]): Document = {
-    val view = SUT(form, NormalMode)
+  private def generateView(form: Form[BigDecimal], mode: Mode): Document = {
+    val view = SUT(form, mode)
     Jsoup.parse(view.toString)
   }
 
   "$className$View" - {
-    "when the form is empty (no errors)" - {
-      val doc = generateView(form)
-      doc.mustHaveCorrectPageTitle(pageHeading)
-      doc.createTestForBackLink(show = true)
-      doc.createTestMustHaveCorrectPageHeading(pageTitle)
+    Mode.values.foreach { mode =>
+      s"when the form is empty (no errors) and the mode is \$mode" - {
+        val doc = generateView(form, mode)
+        doc.mustHaveCorrectPageTitle(pageHeading)
+        doc.createTestForBackLink(show = true)
+        doc.createTestMustHaveCorrectPageHeading(pageTitle)
+        doc.createTestMustShowIsThisPageNotWorkingProperlyLink
 
-      doc.createTestMustShowNumberOfInputs(1)
-      doc.createTestMustShowInput(
-        expectedName = "value",
-        expectedLabel = "$className$",
-        expectedValue = "",
-        expectedHint = None
-      )
+        doc.createTestMustShowNumberOfInputs(1)
+        doc.createTestMustShowInput(
+          expectedName = "value",
+          expectedLabel = "$className$",
+          expectedValue = "",
+          expectedHint = None
+        )
+
+        doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+          controllers.routes.$className$Controller.onSubmit(mode),
+          "Continue"
+        )
+      }
     }
   }
 }
-
 
 object $className$ViewSpec {
   val pageHeading = "$className$"
