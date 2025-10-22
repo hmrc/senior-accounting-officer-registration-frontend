85bba6c1a5f1cccb6c83cd77c88d7d61462dc4fd
Rishi Sachania <19263328+rishisachania@users.noreply.github.com>
2025-10-22 10:55:32 +0100

    adding currencyPage viewspec (#70)
    * adding currencyPage viewspec
    
    * Update $className$ViewSpec.scala
    
    * updates
    
    * Updates to var mapping
    
    * updates to mappings
    
    * refactor: use ViewSpecBase functions
    
    * test: test the mode of the page
    
    * refactor: use combined function for single input
    
    * test: also test filled in form
    
    * test: use minimum property instead of hardcoded value
    
    * fix: handle case where Int.MaxValue is used
    
    * fix: swap test names
    
    * refactor: remove parens
    
    ---------
    
    Co-authored-by: jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>

---
+++ .g8/currencyPage/generated-test/views/$className$ViewSpec.scala
@@ -0,0 +1,79 @@
+
+package views
+
+import base.ViewSpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.data.Form
+import play.api.inject.Injector
+import models.NormalMode
+import pages.$className$Page
+import forms.$className$FormProvider
+import views.html.$className$View
+import views.$className$ViewSpec.*
+import models.Mode
+
+
+class $className$ViewSpec extends ViewSpecBase[$className$View] {
+
+  private val formProvider = app.injector.instanceOf[$className$FormProvider]
+  private val form: Form[BigDecimal] = formProvider()
+
+  private def generateView(form: Form[BigDecimal], mode: Mode): Document = {
+    val view = SUT(form, mode)
+    Jsoup.parse(view.toString)
+  }
+
+  "$className$View" - {
+    Mode.values.foreach { mode =>
+      s"when using \$mode" - {
+        s"when the form is not filled in" - {
+          val doc = generateView(form, mode)
+          doc.mustHaveCorrectPageTitle(pageHeading)
+          doc.createTestForBackLink(show = true)
+          doc.createTestMustHaveCorrectPageHeading(pageTitle)
+          doc.createTestMustShowIsThisPageNotWorkingProperlyLink
+
+          doc.createTestMustShowASingleInput(
+            expectedName = "value",
+            expectedLabel = "$className$",
+            expectedValue = "",
+            expectedHint = None
+          )
+
+          doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+            controllers.routes.$className$Controller.onSubmit(mode),
+            "Continue"
+          )
+        }
+
+        s"when the form is filled in" - {
+          val doc = generateView(form.bind(Map("value" -> testInputValue)), mode)
+          doc.mustHaveCorrectPageTitle(pageHeading)
+          doc.createTestForBackLink(show = true)
+          doc.createTestMustHaveCorrectPageHeading(pageTitle)
+          doc.createTestMustShowIsThisPageNotWorkingProperlyLink
+
+          doc.createTestMustShowASingleInput(
+            expectedName = "value",
+            expectedLabel = "$className$",
+            expectedValue = testInputValue,
+            expectedHint = None
+          )
+
+          doc.createTestMustHaveASubmissionButtonWhichSubmitsTo(
+            controllers.routes.$className$Controller.onSubmit(mode),
+            "Continue"
+          )
+        }
+      }
+    }
+  }
+}
+
+object $className$ViewSpec {
+  val pageHeading = "$className$"
+  val pageTitle = "$className$"
+  val pageCaption = "$className$"
+  val testInputValue = $minimum$.toString
+}
