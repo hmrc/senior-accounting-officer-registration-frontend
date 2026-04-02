616a9be8f6b796ed5504e0b767c512c3f2c06216
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-04-02 10:45:35 +0100

    unauthorised view unit test

---
+++ test/views/UnauthorisedViewSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import base.ViewSpecBase
+import views.html.UnauthorisedView
+import org.jsoup.Jsoup
+import views.UnauthorisedViewSpec.{pageHeading, pageTitle, paragraphs}
+
+class UnauthorisedViewSpec extends ViewSpecBase[UnauthorisedView] {
+  "UnauthorisedView" - {
+    val doc = Jsoup.parse(SUT().toString)
+
+    doc.createTestsWithStandardPageElements(
+      pageTitle = pageTitle,
+      pageHeading = pageHeading,
+      showBackLink = true,
+      showIsThisPageNotWorkingProperlyLink = true,
+      hasError = false
+    )
+
+    doc.createTestsWithParagraphs(paragraphs)
+  }
+}
+
+object UnauthorisedViewSpec {
+  val pageTitle   = "You can’t access this service with this account"
+  val pageHeading = "You can’t access this service with this account"
+  val paragraphs  = List("unauthorised.guidance")
+}
