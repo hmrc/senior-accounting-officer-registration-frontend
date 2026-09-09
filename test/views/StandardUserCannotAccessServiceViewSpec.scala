d644c0d58d587b78605edfccb9028de755f0f660
calebnartey <72252764+calebnartey@users.noreply.github.com>
2026-09-09 16:38:16 +0100

    tweaks

--- test/views/StandardUserCannotAccessServiceViewSpec.scala
+++ test/views/StandardUserCannotAccessServiceViewSpec.scala
@@ -60,8 +60,8 @@ object StandardUserCannotAccessServiceViewSpec {
   val pageHeading = "You cannot access this service"
   val pageTitle   = "You cannot access this service"
   val paragraph1  =
-    """You’ve signed in using your organisation Government Gateway ID with a standard user role. Only users with an administrator role can use this service."""
+    """You’ve signed in using your organisation Government Gateway ID as an assistant with a standard user role. Only users with an administrator role can use this service."""
   val signInLinkText     = "sign in using an organisation Government Gateway ID"
   val paragraph2: String =
-    s"If you need to register, $signInLinkText that has the Administrator role, or ask an administrator to add you as a team member with the Administrator role."
+    s"If you need to register, $signInLinkText hat has the administrator role, or ask an administrator to add you as a team member with the administrator role"
 }
