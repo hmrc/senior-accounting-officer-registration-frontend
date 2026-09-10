d006e8b53a777e72193c89e63db4693f4de8c53b
Caleb Nartey <72252764+calebnartey@users.noreply.github.com>
2026-09-10 16:28:42 +0100

    filtered errors (#155)

--- app/viewmodels/govuk/ErrorSummaryFluency.scala
+++ app/viewmodels/govuk/ErrorSummaryFluency.scala
@@ -32,7 +32,7 @@ trait ErrorSummaryFluency {
         errorLinkOverrides: Map[String, String] = Map.empty
     )(using messages: Messages): ErrorSummary = {
 
-      val errors = form.errors.map { error =>
+      val errors = form.errors.headOption.map { error =>
         ErrorLink(
           href = Some(s"#${errorLinkOverrides.getOrElse(error.key, error.key)}"),
           content = Text(messages(error.message, error.args*))
@@ -40,7 +40,7 @@ trait ErrorSummaryFluency {
       }
 
       ErrorSummary(
-        errorList = errors,
+        errorList = errors.toSeq,
         title = Text(messages("error.summary.title"))
       )
     }
